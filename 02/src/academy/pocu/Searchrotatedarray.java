c79f592d0501a1a8b7189f7ae15febab66464d13
dexter <dex9205@gmail.com>
2021-10-12 16:13:15 +0900

    회전 탐색

---
+++ 02/src/academy/pocu/Searchrotatedarray.java
@@ -0,0 +1,49 @@
+package src.academy.pocu;
+
+public class Searchrotatedarray {
+    public static void main(String[] args) {
+        int[] arry = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
+
+        int index = indexOfRotatedArray(arry, 0, arry.length - 1, 0);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 1);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 29);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 6);
+
+        System.out.println(index);
+    }
+
+    private static int indexOfRotatedArray(int[] arry, int start, int end, int num) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (arry[mid] == num) {
+            return mid;
+        }
+
+        if (arry[start] <= arry[mid]) {
+            if (num >= arry[start] && num <= arry[mid]) {
+                return indexOfRotatedArray(arry, start, mid - 1, num);
+            }
+
+            return indexOfRotatedArray(arry, mid + 1, end, num);
+        }
+
+        if (num >= arry[mid] && num <= arry[end]) {
+            return indexOfRotatedArray(arry, mid + 1, end, num);
+        }
+
+        return indexOfRotatedArray(arry, start, mid - 1, num);
+    }
+}
