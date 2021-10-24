bf065f445bc9ab47f71ecb41f9683ebbc0ac55b9
dexter <dex9205@gmail.com>
2021-10-24 17:18:26 +0900

    실습

---
+++ src/academy/pocu/ch08/Directorytree.java
@@ -0,0 +1,47 @@
+package academy.pocu.ch08;
+
+import java.io.File;
+
+public class Directorytree {
+    private static final int INDENT_LENGTH = 2;
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println(String.format("Wrong number of arguments: %d", args.length));
+            System.exit(1);
+        }
+
+        String path = args[0];
+        File file = new File(path);
+
+        printDirectoryTreeRecursive(file, 0);
+    }
+
+    private static void printDirectoryTreeRecursive(File file, int depth){
+        String filename = file.getName();
+
+        String message = "- " + filename;
+        message = padLeft(INDENT_LENGTH * depth, message);
+
+        System.out.println(message);
+
+        if(file.isDirectory()){
+            File[] children = file.listFiles();
+
+            for(File child : children){
+                printDirectoryTreeRecursive(child, depth + 1);
+            }
+        }
+    }
+
+    private static String padLeft(int padLength, String message){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < padLength; i++){
+            sb.append(' ');
+        }
+
+        sb.append(message);
+
+        return sb.toString();
+    }
+}
