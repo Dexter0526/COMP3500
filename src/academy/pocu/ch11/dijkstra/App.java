bb9c36f4f428845178f31fa7cbf9f2334959d819
dexter <dex9205@gmail.com>
2021-11-02 17:06:35 +0900

    다익스트라 알고리즘 샘플 코드

---
+++ src/academy/pocu/ch11/dijkstra/App.java
@@ -0,0 +1,77 @@
+package academy.pocu.ch11.dijkstra;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) {
+        HashMap<String, Node> nodes = createNodes();
+
+        HashMap<String, String> prevs = new HashMap<>();
+
+        HashMap<String, Integer> minDists = Dijkstra.run(nodes, "Home", prevs);
+
+        int schoolDist = minDists.get("School");
+        System.out.println(schoolDist);
+
+        int bankDist = minDists.get("Bank");
+        System.out.println(bankDist);
+
+        int libDists = minDists.get("Library");
+        System.out.println(libDists);
+
+        LinkedList<String> path = new LinkedList<>();
+
+        String name = "School";
+        while (name != null) {
+            path.addFirst(name);
+            name = prevs.get(name);
+        }
+
+        String pathString = String.join(" -> ", path);
+
+        System.out.println(pathString);
+
+    }
+
+    private static HashMap<String, Node> createNodes() {
+        Node home = new Node("Home");
+        Node policeStation = new Node("Police Station");
+        Node school = new Node("School");
+        Node park = new Node("Park");
+        Node bank = new Node("Bank");
+        Node library = new Node("Library");
+
+        home.addRoad(policeStation, 2);
+        policeStation.addRoad(home, 2);
+
+        home.addRoad(park, 3);
+        park.addRoad(home, 3);
+
+        policeStation.addRoad(bank, 1);
+        bank.addRoad(policeStation, 1);
+
+        policeStation.addRoad(school, 6);
+        school.addRoad(policeStation, 6);
+
+        bank.addRoad(library, 2);
+        library.addRoad(bank, 2);
+
+        bank.addRoad(park, 2);
+        park.addRoad(bank, 2);
+
+        school.addRoad(library, 1);
+        library.addRoad(school, 1);
+
+        HashMap<String, Node> nodes = new HashMap<>();
+
+        nodes.put(home.getName(), home);
+        nodes.put(policeStation.getName(), policeStation);
+        nodes.put(school.getName(), school);
+        nodes.put(park.getName(), park);
+        nodes.put(bank.getName(), bank);
+        nodes.put(library.getName(), library);
+
+        return nodes;
+    }
+}
---
+++ src/academy/pocu/ch11/dijkstra/Dijkstra.java
@@ -0,0 +1,75 @@
+package academy.pocu.ch11.dijkstra;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    private Dijkstra(){
+
+    }
+
+    public static HashMap<String, Integer> run(final HashMap<String, Node> nodes, final String from, final HashMap<String, String> prevs){
+//        해당 노드까지 최소 거리
+        HashMap<String, Integer> minDists = new HashMap<>();
+
+        final int INF = Integer.MAX_VALUE;
+
+//        거리 초기화(무한대)
+        for(var entry : nodes.entrySet()){
+            String name = entry.getKey();
+
+            minDists.put(name, INF);
+        }
+
+//        시작점
+        minDists.put(from, 0);
+        prevs.put(from, null);
+
+        PriorityQueue<Candidate> open = new PriorityQueue<>();
+
+//        루트 노드
+        Node s = nodes.get(from);
+        Candidate candidate = new Candidate(s, 0);
+
+        open.add(candidate);
+
+        while (!open.isEmpty()){
+            candidate = open.poll();
+
+            Node n = candidate.getNode();
+
+            String nodeName = n.getName();
+
+            int minDist = minDists.get(nodeName);
+            int dist = candidate.getDistance();
+
+            if(minDist < dist) continue;
+
+//            인접 노드
+            Map<Node, Integer> roads = n.getRoads();
+
+            for(var e : roads.entrySet()){
+                Node next = e.getKey();
+
+                int weight = e.getValue();
+                int newDist = minDist + weight;
+
+                String nextName = next.getName();
+                int nextMinDist = minDists.get(nextName);
+
+                if(newDist > nextMinDist) continue;
+
+                minDists.put(nextName, newDist);
+                prevs.put(nextName, nodeName);
+
+                Candidate newCandidate = new Candidate(next, newDist);
+
+                open.add(newCandidate);
+            }
+        }
+
+        return minDists;
+        
+    }
+}
